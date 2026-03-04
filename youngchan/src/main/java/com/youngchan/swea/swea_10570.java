572d186a734f2abf223ef51b4f38b063a7c40fb0
CHAN <150508884+zerochani@users.noreply.github.com>
2026-03-04 18:28:51 +0900

    swea 10570 제곱 팰린드롬 수

---
+++ youngchan/src/main/java/com/youngchan/swea/swea_10570.java
@@ -0,0 +1,34 @@
+package com.youngchan.swea;
+import java.io.*;
+import java.util.*;
+public class swea_10570 {
+    static boolean isPalindrome(int x){
+        String s = String.valueOf(x);
+        String r = new StringBuilder(s).reverse().toString();
+        return s.equals(r);
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        for(int i=1; i<=tc; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int count = 0;
+
+            int start = (int)Math.sqrt(a);
+            int end = (int)Math.sqrt(b);
+
+            for(int j=start; j<=end; j++){
+                if(isPalindrome(j)){
+                    int square = j*j;
+                    if(square>=a && square<=b && isPalindrome(square)){
+                        count++;
+                    }
+                }
+            }
+            System.out.println("#" + i + " " + count);
+        }
+    }
+}
