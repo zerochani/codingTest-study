e69e6bbf7d1201af1a396e10ee910c8dcfebb3ea
CHAN <150508884+zerochani@users.noreply.github.com>
2026-04-16 19:06:37 +0900

    swea 1215 회문1

--- youngchan/src/main/java/com/youngchan/swea/swea_1215.java
+++ youngchan/src/main/java/com/youngchan/swea/swea_1215.java
@@ -1,41 +1,41 @@
 package com.youngchan.swea;
-import java.io.*;
 import java.util.*;
-public class swea_1215 {
-    static boolean isRowPalindrome(char[][] ch, int row, int start, int len){
-        for(int k=0; k<len/2; k++){
-            if(ch[row][start+k] != ch[row][start+len-1-k]){
-                return false;
-            }
-        }
-        return true;
-    }
 
-    static boolean isColPalindrome(char[][] ch, int col, int start, int len){
-        for(int k=0; k<len/2; k++){
-            if(ch[start+k][col] != ch[start+len-1-k][col]){
-                return false;
-            }
-        }
-        return true;
-    }
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class swea_1215 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         for(int tc=1; tc<=10; tc++){
-            int n = Integer.parseInt(br.readLine());
-            char[][] ch = new char[8][8];
+            int n = sc.nextInt();
+            char[][] arr = new char[8][8];
             for(int i=0; i<8; i++){
-                String s = br.readLine();
-                for(int j=0; j<8; j++){
-                    ch[i][j] = s.charAt(j);
-                }
+                arr[i] = sc.next().toCharArray();
             }
 
             int cnt = 0;
+            //가로 검사
+            for(int i=0; i<8; i++){
+                for(int j=0; j<=8-n; j++){ //n이 8일때도 고려
+                    boolean flag = true;
+                    for(int k=0; k<n/2; k++){
+                        if(arr[i][j+k] != arr[i][j+n-1-k]){
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if(flag) cnt++;
+                }
+            }
+            //세로 검사
             for(int i=0; i<8; i++){
                 for(int j=0; j<=8-n; j++){
-                    if(isRowPalindrome(ch, i, j, n)) cnt++;
-                    if(isColPalindrome(ch, i, j, n)) cnt++;
+                    boolean flag = true;
+                    for(int k=0; k<n/2; k++){
+                        if(arr[j+k][i] != arr[j+n-1-k][i]){
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if(flag) cnt++;
                 }
             }
             System.out.println("#" + tc + " " + cnt);
