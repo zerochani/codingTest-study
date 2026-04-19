6c9f7903f43bead6a2a441b24f3f208e62ee7951
CHAN <150508884+zerochani@users.noreply.github.com>
2026-04-19 16:32:08 +0900

    swea 1238 Contact

--- youngchan/src/main/java/com/youngchan/swea/swea_1238.java
+++ youngchan/src/main/java/com/youngchan/swea/swea_1238.java
@@ -1,56 +1,55 @@
 package com.youngchan.swea;
 import java.util.*;
 import java.io.*;
+
 public class swea_1238 {
-    static final int MAX = 101;
     static int[][] arr;
-    static int[] visited;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         for(int tc=1; tc<=10; tc++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int len = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
             int start = Integer.parseInt(st.nextToken());
-            arr = new int[MAX][MAX];
-            visited = new int[MAX];
+            arr = new int[101][101];
             st = new StringTokenizer(br.readLine());
-            for(int i=0; i<len/2; i++){
+            for(int i=0; i<n/2; i++){
                 int from = Integer.parseInt(st.nextToken());
                 int to = Integer.parseInt(st.nextToken());
                 arr[from][to] = 1;
             }
 
-            int result = bfs(start);
-            System.out.println("#" + tc + " " + result);
+            System.out.println("#" + tc + " " + bfs(start));
         }
     }
     static int bfs(int start){
         Queue<Integer> q = new LinkedList<>();
+        int[] visited = new int[101];
         q.offer(start);
         visited[start] = 1;
 
-        int maxDepth = 1;
-        int maxNode = start;
-
+        int maxLevel = 1;
+        int answer = start;
         while(!q.isEmpty()){
             int cur = q.poll();
 
-            for(int i=1; i<MAX; i++){
-                //연락 가능한 경로가 있고, 아직 방문하지 않은 경우
-                if(arr[cur][i] == 1 && visited[i] ==0){
-                    visited[i] = visited[cur] + 1; //방문 체크 및 연락 횟수 증가
-                    q.offer(i);
+            //현재 노드 단계보다 높은 단계를 만나면 갱신
+            if(visited[cur] > maxLevel){
+                maxLevel = visited[cur];
+                answer = cur;
+            }
 
-                    //마지막 단계의 노드 정보 갱신
-                    if(visited[i] > maxDepth){
-                        maxDepth = visited[i];
-                        maxNode = i;
-                    }else if(visited[i] == maxDepth){
-                        maxNode = Math.max(maxNode, i); //같은 단계의 노드 중 번호가 큰 노드 선택
-                    }
+            //가장 마지막 단계에서 가장 숫자가 큰 노드 찾기
+            else if(visited[cur] == maxLevel){
+                answer = Math.max(answer, cur);
+            }
+
+            for(int next=1; next<=100; next++){
+                if(arr[cur][next] == 1 && visited[next] ==0){
+                    visited[next] = visited[cur] + 1;
+                    q.offer(next);
                 }
             }
         }
-        return maxNode;
+        return answer;
     }
 }
